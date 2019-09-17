c7f1531d0a494340dc49e220a57ceed05760acd7
章理 <790847400@qq.com>
2019-09-17 18:00:46 +0800

    检测是否支持 emoji 表情包

--- src/components/FaceList.vue
+++ src/components/FaceList.vue
@@ -43,7 +43,24 @@ export default {
       currentTab: 'qq'
     }
   },
+  created () {
+    console.log('emojiSupported', this.emojiSupported())
+  },
   methods: {
+    emojiSupported () {
+      var node = document.createElement('canvas')
+      if (!node.getContext ||
+          !node.getContext('2d') ||
+          typeof node.getContext('2d').fillText !== 'function'
+      ) {
+        return false
+      }
+      var ctx = node.getContext('2d')
+      ctx.textBaseline = 'top'
+      ctx.font = '32px Arial'
+      ctx.fillText('\ud83d\ude03', 0, 0)
+      return ctx.getImageData(16, 16, 1, 1).data[0] !== 0
+    },
     onClickFace () {
     },
     onDelFace () {
